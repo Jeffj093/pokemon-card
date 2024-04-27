d6590e42208a82b117b984d55b955dfcd5fd7c67
Jeff <jeffj0092@gmail.com>
2024-04-27 00:04:53 -0400

    some updates

--- index.js
+++ index.js
@@ -3,16 +3,33 @@ const img = document.getElementById("pokeimg")
 const namePokemon = document.getElementById("pokename")
 const superType = document.getElementById("supertypes")
 const subType = document.getElementById("subtypes")
+const healthPercent = document.getElementById("pokehealth")
+// const pokeDmg = document.getElementById("pokedmg")
+const powerAtk1 = document.getElementById("pokeatk1")
+const powerDesc1 = document.getElementById("pokedesc1")
+const powerAtk2 = document.getElementById("pokeatk2")
+const powerDesc2 = document.getElementById("pokedesc2")
+
 
 function getApiData() {
-  fetch("https://api.pokemontcg.io/v2/cards/base1-1")
+  fetch("https://api.pokemontcg.io/v2/cards/base3-33")
   .then (response => response.json())
   .then (data => {
       img.src = data.data.images.large
       namePokemon.textContent = data.data.name
       superType.textContent = data.data.supertype
       subType.textContent = data.data.subtypes
+      healthPercent.textContent = data.data.hp
+      // pokeDmg.textContent = data.data.attacks[1].damage
+      powerAtk1.textContent = data.data.attacks[0].name
+      powerDesc1.textContent = data.data.attacks[0].text
+      powerAtk2.textContent = data.data.attacks[1].name
+      powerDesc2.textContent= data.data.attacks[1].text
+      
 
+      
+      
+  // console.log(data.data.attacks[1].cost.damage);
   });
 
 };

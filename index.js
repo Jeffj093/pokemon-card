a6fe3e594a8d3983b9d666183a741e6009be63e0
Jeff <jeffj0092@gmail.com>
2024-04-27 12:09:13 -0400

    wfk

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const namePokemon = document.getElementById("pokename")
 const superType = document.getElementById("supertypes")
 const subType = document.getElementById("subtypes")
 const healthPercent = document.getElementById("pokehealth")
+const powerType = document.getElementById("powrtype")
 // const pokeDmg = document.getElementById("pokedmg")
 const powerAtk1 = document.getElementById("pokeatk1")
 const powerDesc1 = document.getElementById("pokedesc1")
@@ -12,7 +13,7 @@ const powerDesc2 = document.getElementById("pokedesc2")
 
 
 function getApiData() {
-  fetch("https://api.pokemontcg.io/v2/cards/base3-33")
+  fetch("https://api.pokemontcg.io/v2/cards/base5-38")
   .then (response => response.json())
   .then (data => {
       img.src = data.data.images.large
@@ -20,6 +21,7 @@ function getApiData() {
       superType.textContent = data.data.supertype
       subType.textContent = data.data.subtypes
       healthPercent.textContent = data.data.hp
+      powerType.src = `public/img/${data.data.types[0]}.webp`
       // pokeDmg.textContent = data.data.attacks[1].damage
       powerAtk1.textContent = data.data.attacks[0].name
       powerDesc1.textContent = data.data.attacks[0].text

1ecaa0fd8ff2d9278ca3ae7b87351d51d4c5f9ee
Jeff <jeffj0092@gmail.com>
2024-05-04 01:19:23 -0400

    some updates

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const namePokemon = document.getElementById("pokename")
 const superType = document.getElementById("supertypes")
 const subType = document.getElementById("subtypes")
 const healthPercent = document.getElementById("pokehealth")
-// const pokeDmg = document.getElementById("pokedmg")
+const pokeDmg = document.getElementById("pkdmg")
 const powerAtk1 = document.getElementById("pokeatk1")
 const powerDesc1 = document.getElementById("pokedesc1")
 const powerAtk2 = document.getElementById("pokeatk2")
 const powerDesc2 = document.getElementById("pokedesc2")
+const powerMarbles = document.getElementById("pepas")
 
 
 function getApiData() {
@@ -20,16 +21,25 @@ function getApiData() {
       superType.textContent = data.data.supertype
       subType.textContent = data.data.subtypes
       healthPercent.textContent = data.data.hp
-      // pokeDmg.textContent = data.data.attacks[1].damage
+      pokeDmg.textContent = data.data.attacks[0].damage
       powerAtk1.textContent = data.data.attacks[0].name
       powerDesc1.textContent = data.data.attacks[0].text
       powerAtk2.textContent = data.data.attacks[1].name
       powerDesc2.textContent= data.data.attacks[1].text
+
       
+      const pepas = data.data.attacks[1].cost
+      console.log(data.data.attacks[1].cost)
+      pepas.forEach(items => {
+      const img = document.createElement('img');
+      img.src = `public/img/${items}.webp`
+      powerMarbles.appendChild(img); 
+        
+      });
 
       
       
-  // console.log(data.data.attacks[1].cost.damage);
+  // console.log(data.data.attacks[1].cost);
   });
 
 };

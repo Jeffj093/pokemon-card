1d3f028a2ad713a809571d835ab84d2da1604828
Jeff <jeffj0092@gmail.com>
2024-05-09 02:25:23 -0400

    last changes jeff

--- app.js
+++ app.js
@@ -4,12 +4,8 @@ const namePokemon = document.getElementById("pokename")
 const superType = document.getElementById("supertypes")
 const subType = document.getElementById("subtypes")
 const healthPercent = document.getElementById("pokehealth")
-<<<<<<< HEAD:app.js
 const pokeDmg = document.getElementById("pkdmg")
-=======
 const powerType = document.getElementById("powrtype")
-// const pokeDmg = document.getElementById("pokedmg")
->>>>>>> a6fe3e594a8d3983b9d666183a741e6009be63e0:index.js
 const powerAtk1 = document.getElementById("pokeatk1")
 const powerDesc1 = document.getElementById("pokedesc1")
 const powerAtk2 = document.getElementById("pokeatk2")
@@ -18,7 +14,7 @@ const powerMarbles = document.getElementById("pepas")
 
 
 function getApiData() {
-  fetch("https://api.pokemontcg.io/v2/cards/base5-38")
+  fetch("https://api.pokemontcg.io/v2/cards/base3-33")
   .then (response => response.json())
   .then (data => {
       img.src = data.data.images.large
@@ -26,32 +22,42 @@ function getApiData() {
       superType.textContent = data.data.supertype
       subType.textContent = data.data.subtypes
       healthPercent.textContent = data.data.hp
-<<<<<<< HEAD:app.js
       pokeDmg.textContent = data.data.attacks[0].damage
-=======
-      powerType.src = `public/img/${data.data.types[0]}.webp`
-      // pokeDmg.textContent = data.data.attacks[1].damage
->>>>>>> a6fe3e594a8d3983b9d666183a741e6009be63e0:index.js
       powerAtk1.textContent = data.data.attacks[0].name
       powerDesc1.textContent = data.data.attacks[0].text
       powerAtk2.textContent = data.data.attacks[1].name
       powerDesc2.textContent= data.data.attacks[1].text
 
-      
-      const pepas = data.data.attacks[1].cost
-      console.log(data.data.attacks[1].cost)
-      pepas.forEach(items => {
-      const img = document.createElement('img');
-      img.src = `public/img/${items}.webp`
-      powerMarbles.appendChild(img); 
+      powerType.src = `public/img/${data.data.types[0]}.webp`
+
+      const pepas = data.data.attacks[0].cost
+        // console.log(data.data.attacks[0].cost)
+
+        pepas.forEach((items, index) => {
+            if(index == 0) {
+                  powerMarbles.textContent = ''
+            }
+      const img2 = document.createElement('img');
+      img2.src = `public/img/${items}.webp`
+      powerMarbles.appendChild(img2); 
         
       });
-
-      
-      
-  // console.log(data.data.attacks[1].cost);
   });
 
 };
 
 button.addEventListener("click", getApiData)
+
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+
+toggleBtn.onclick = function (){
+  dropDownMenu.classList.toggle('open')
+  const isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList =isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
+
+}
